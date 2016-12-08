f5d1926ce7af915c6827540c5267665b39d11de6
Ian <bunkadoo91@gmail.com>
2016-12-08 15:26:38 -0800

    cant add uless u log in

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesTracker.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalesTracker.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult NewItem(int Id, string newName, string newDescription, decimal newCost, int newQuantity)
+        {
+            Item newItem = new Item(newName, newDescription, newCost, newQuantity, Id);
+            db.Items.Add(newItem);
+            db.SaveChanges();
+            return Json(newItem);
+        }
+    }
+}
